4b14b3231a08ca670d2bd23dc4fe306f118e83cc
Roel de Jong <Roel4811@hotmail.com>
2019-04-12 22:22:10 +0200

    Update tests

--- energy/test/certficates.js
+++ energy/test/certficates.js
@@ -1,32 +1,27 @@
 var Certificates = artifacts.require("./Certificates.sol")
 
 contract('PanelAuction', accounts => {
-  var certificatesInstance
+  let certificatesInstance
 
   const userOne = accounts[0]
   const userTwo = accounts[1]
 
-  it("should deploy the contract", () => {
-    return Certificates.deployed().then(instance => {
-      certificatesInstance = instance
-    })
+  it("should deploy the contract", async () => {
+    certificatesInstance = await Certificates.deployed();
   })
 
-  it("should push cetrificates", () => {
+  it("should push cetrificates", async () => {
     const id = 1000001
     const amount = 100
     const timestamp = Math.floor(Date.now() / 1000)
     const certificateType = 0
 
-    return certificatesInstance.pushCertificate(id, amount, timestamp, certificateType, {from: userOne})
-    .then(() => {
-      return certificatesInstance.certificates.call(id)
-    })
-    .then(certificates => {
-      console.log(certificates)
-      assert.equal(certificates[0][0], amount, "Wrong amount")
-      assert.equal(certificates[1][0], timestamp, "Wrong timestamp")
-      assert.equal(certificates[2][0], certificateType, "Wrong certificate type")
-    })
+    await certificatesInstance.pushCertificate(id, amount, timestamp, certificateType, {from: userOne})
+    const certificates = await certificatesInstance.certificates.call(id)
+
+    console.log(certificates)
+    assert.equal(certificates[0][0], amount, "Wrong amount")
+    assert.equal(certificates[1][0], timestamp, "Wrong timestamp")
+    assert.equal(certificates[2][0], certificateType, "Wrong certificate type")
   })
 })
