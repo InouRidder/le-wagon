608daae612a5c9dee0f1203674508a0346382100
Inou Ridder <inouridder@gmail.com>
2019-04-13 18:03:46 +0200

    install tests

---
+++ rails-api/app/services/green_energy_processor.rb
@@ -0,0 +1,44 @@
+class GreenEnergyProcessor
+
+  attr_reader :total_returned_energy
+
+  def initialize(household)
+    @household = attributes[:household]
+    @is_pushed = false
+  end
+
+  def by_datetimes(begin_datetime, end_datetime)
+    @day_radiation_value = RadiationAPIService.weather_between_datetimes(begin_datetime, end_datetime)
+    @dataset = @household.energy_data.uncertified.where(datetime: begin_datetime..end_datetime)
+    @total_returned_energy = @dataset.pluck(:returned_energy).sum
+    to_chain
+  end
+
+  def consumed_green_energy
+    @consumed ||= if @total_returned_energy.zero?
+      ideal_return_energy
+    else
+      ideal_return_energy - @total_returned_energy
+    end
+  end
+
+  def ideal_return_energy
+    @ideal_val ||= @household.peak_returned_energy * @day_radiation_value / @household.peak_radiation
+  end
+
+  def to_chain
+    return if @is_pushed
+    @block_chain = Web3Service.new
+    push_to_chain(consumed_green_energy, 'NTGO')
+    push_to_chain(@total_returned_energy, 'TGO')
+    @dataset.update_all(certified: true)
+  end
+
+  private
+
+  def push_to_chain(value, type_of_cert)
+    if value > 0
+      @block_chain.push_certificate(@household.uid, value, Time.now, type_of_cert)
+    end
+  end
+end
---
+++ rails-api/spec/units/services/green_energy_processor_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe GreenEnergyProcessor do
+  describe "lal" do
+    it "sdfsdf" do
+      puts "teest yall"
+    end
+  end
+end
