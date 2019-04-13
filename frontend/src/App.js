44583815119c1ccf9d0ee70d684cf225104922fe
Roel de Jong <Roel4811@hotmail.com>
2019-04-13 14:54:40 +0200

    Add react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import 'bulma/css/bulma.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <div className="section has-text-centered">
-            <h1 className="title">Hello</h1>
-          </div>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">Certificates</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/topics" component={Topics} />
         </div>
-      </div>
+      </Router>
     );
   }
 }
