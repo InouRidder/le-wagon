4c4866310a2f25dce78c76ae18dc3424c2497004
Inou Ridder <inouridder@gmail.com>
2019-04-13 21:21:13 +0200

    Add rspec and factory bot to test the green energy processor, without testing the chain

--- rails-api/app/services/radiation_API_service.rb
+++ rails-api/app/services/radiation_API_service.rb
@@ -1,6 +1,6 @@
 class RadiationAPIService
   class << self
-    def weather_between_datetimes(begin_datetime, end_datetime, household)
+    def weather_between_datetimes(begin_datetime, end_datetime)
       # url = "https://developer.nrel.gov/api/solar/solar_resource/v1.format?api_key=kUJgs2bV92fhQLV1eMGw89wTzfyFMLPpZv0DBxAg&lat=#{@household.latitude}&lng=#{@household.longitude}";
       # response = HTTParty.get(url)
       [404, 400, 2, 3, 323, 345, 123, 120].sample
